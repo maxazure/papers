d58267d86a522316f2a32128d9f9c82feee08bcc
maxazure <maxazure@gmail.com>
2020-05-08 22:48:46 +1200

    selects

--- resourses/selects_resource.py
+++ resourses/selects_resource.py
@@ -6,14 +6,17 @@ from flask_jwt_extended import jwt_required
 from models.course import Course
 from app import db
 from utils.util import max_res
-
+import json
 
 
 class SelectsResource(Resource):
-
     @jwt_required
     def get(self, course_id=None):
-        res = Course.query.all
+        res = Course.query.all()
+        res_list = []
 
-        return max_res(res)
+        if (res != None):
+            for item in res:
+                res_list.append({'id': item.id, 'name': item.name})
 
+        return max_res(res_list)
